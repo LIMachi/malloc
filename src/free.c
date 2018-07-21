afe0c5d8d07e2b485b2e7e68d3b2cdb5a64826cb
LIMachi <lee.mach.hm@gmail.com>
2018-07-21 20:06:39 +0200

    ajout de free, premiers test (makefile) corrects

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: hmartzol <hmartzol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 19:49:37 by hmartzol          #+#    #+#             */
-/*   Updated: 2018/07/21 19:54:41 by hmartzol         ###   ########.fr       */
+/*   Updated: 2018/07/21 20:05:09 by hmartzol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 
 void	ma_free(t_ma_found_link *f)
 {
-	(void)f;
+	f->found->allocated = 0;
+	if (f->before != NULL && !f->before->allocated)
+	{
+		f->before->size += f->found->size + sizeof(t_ma_link);
+		f->found = f->before;
+	}
+	if (f->after != NULL && !f->after->allocated)
+	{
+		f->found->size += f->after->size + sizeof(t_ma_link);
+		f->after = f->found;
+	}
 }
 
 void	free(void *ptr)
 {
-	(void)ptr;
+	t_ma_found_link	f;
+
+	if (ptr == NULL || !ma_validate_pointer(ptr, &f) || !f.found->allocated)
+		return ;
+	ma_free(&f);
 }
